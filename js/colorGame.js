d8cf7211f93b115130eb5811e937899482ac5a50
Alcides <eojedapilchik@gmail.com>
2020-10-07 08:31:17 -0400

    added random color generation

--- js/colorGame.js
+++ js/colorGame.js
@@ -1,12 +1,14 @@
 // alert("Connected");
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-];
+// let colors = [
+//   "rgb(255, 0, 0)",
+//   "rgb(255, 255, 0)",
+//   "rgb(0, 255, 0)",
+//   "rgb(0, 255, 255)",
+//   "rgb(0, 0, 255)",
+//   "rgb(255, 0, 255)",
+// ];
+
+let colors = generateRandomColors(6);
 
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
@@ -42,4 +44,19 @@ function changeColors(color) {
 function pickColor() {
   let rnd = Math.floor(Math.random() * colors.length);
   return colors[rnd];
+}
+
+function generateRandomColors(num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
